e781ecde4f32f407d135d1d760a536e19b74c782
Patrick Nguyen <iamtiredofyou2@gmail.com>
2022-06-27 11:19:14 -0400

    Project setup

---
+++ components/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const CustomStyles = createGlobalStyle`
+
+`;
+
+const GlobalStyles = () => {
+	return <CustomStyles />;
+};
+
+export default GlobalStyles;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import GlobalStyles from '../components/GlobalStyles/GlobalStyles';
 
-export default MyApp
+import '../styles/globals.css';
+
+const App = ({ Component, pageProps }: AppProps) => {
+	return (
+		<>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</>
+	);
+};
+
+export default App;
